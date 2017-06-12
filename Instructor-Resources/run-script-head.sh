c739c3ba26eddb00e920bc8f7e2ea7d4a589f792
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-06-12 06:47:46 -0700

    fix script action

--- Instructor-Resources/run-script-head.sh
+++ Instructor-Resources/run-script-head.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+cd /tmp
 mkdir data && cd data
 wget https://alizaidi.blob.core.windows.net/training/data/yelp/yelp_dataset_challenge_round9.tar
 tar -xvf yelp_dataset_challenge_round9.tar
-cd ..
+cd ../
 hdfs dfs -mkdir /yelp
 hdfs dfs -copyFromLocal data/ /yelp
 
 ## download notebooks
 
 git clone https://github.com/Azure/learnAnalytics-SparkML.git
+chown -R sshuser:sshuser ./learnAnalytics-SparkML
 cd learnAnalytics-SparkML/Student-Resources
-hdfs dfs -copyFromLocal Labs HdiNotebooks/
+hdfs dfs -copyFromLocal Labs/ /HdiNotebooks/
